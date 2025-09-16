f33f7423d6cee6d3e7610e231e5e359a74ad1036
Suleymanova Seltenet Celal <suley_kn06@itstep.edu.az>
2025-09-16 11:52:24 +0400

    Commit mesajı burada

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,10 +1,23 @@
+# from django.urls import path
+# from . import views
+#
+# urlpatterns = [
+#     path('', views.home, name='home'),
+#     path('product/<slug:slug>/', views.product_detail, name='product_detail'),
+#     path('add/', views.add_product, name='add_product'),
+#     path('cart/', views.cart_view, name='cart'),
+#     path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+# ]
+
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('product/<slug:slug>/', views.product_detail, name='product_detail'),
-    path('add/', views.add_product, name='add_product'),
+    path('add-product/', views.add_product, name='add_product'),
     path('cart/', views.cart_view, name='cart'),
-    path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('update-cart/<int:product_id>/<str:action>/', views.update_cart, name='update_cart'),
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,7 +1,60 @@
+# from django.shortcuts import render, get_object_or_404, redirect
+# from .models import Product
+# from django.http import HttpResponseRedirect
+# from django.urls import reverse
+#
+#
+# def home(request):
+#     products = Product.objects.all()
+#     return render(request, 'home.html', {'products': products})
+#
+#
+# def product_detail(request, slug):
+#     product = get_object_or_404(Product, slug=slug)
+#     return render(request, 'product_detail.html', {'product': product})
+#
+#
+# def add_product(request):
+#     if request.method == 'POST':
+#         name = request.POST['name']
+#         price = request.POST['price']
+#         quantity = request.POST['quantity']
+#         image_url = request.POST['image_url']
+#         Product.objects.create(name=name, price=price, quantity=quantity, image_url=image_url)
+#         return redirect('home')
+#     return render(request, 'add_product.html')
+#
+#
+# def add_to_cart(request, product_id):
+#     cart = request.session.get('cart', {})
+#     cart[product_id] = cart.get(product_id, 0) + 1
+#     request.session['cart'] = cart
+#     return HttpResponseRedirect(reverse('cart'))
+#
+# def cart_view(request):
+#     cart = request.session.get('cart', {})
+#     cart_items = []
+#     total = 0
+#
+#     for product_id, qty in cart.items():
+#         product = Product.objects.get(id=product_id)
+#         subtotal = product.price * qty
+#         total += subtotal
+#         cart_items.append({
+#             'product': product,
+#             'qty': qty,
+#             'subtotal': subtotal
+#         })
+#
+#     return render(request, 'cart.html', {'cart_items': cart_items, 'total': total})
+#
+
+
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib import messages
 
 
 def home(request):
@@ -25,12 +78,28 @@ def add_product(request):
     return render(request, 'add_product.html')
 
 
+# Səbətə əlavə etmək
 def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+
+    if product.quantity <= 0:
+        messages.error(request, 'This product is out of stock.')
+        return redirect('home')
+
     cart = request.session.get('cart', {})
-    cart[product_id] = cart.get(product_id, 0) + 1
+
+    # Əgər məhsul artıq səbətdə varsa, quantity-ni artır
+    cart[product_id] = cart.get(str(product_id), 0) + 1
     request.session['cart'] = cart
+
+    # Məhsul stokunu azaldırıq
+    product.quantity -= 1
+    product.save()
+
     return HttpResponseRedirect(reverse('cart'))
 
+
+# Səbəti göstərmək
 def cart_view(request):
     cart = request.session.get('cart', {})
     cart_items = []
@@ -48,3 +117,23 @@ def cart_view(request):
 
     return render(request, 'cart.html', {'cart_items': cart_items, 'total': total})
 
+
+
+def update_cart(request, product_id, action):
+    product = get_object_or_404(Product, id=product_id)
+    cart = request.session.get('cart', {})
+
+    if str(product_id) in cart:
+        if action == 'increase' and product.quantity > 0:
+            cart[str(product_id)] += 1
+            product.quantity -= 1
+            product.save()
+        elif action == 'decrease':
+            cart[str(product_id)] -= 1
+            product.quantity += 1
+            product.save()
+            if cart[str(product_id)] <= 0:
+                del cart[str(product_id)]
+
+    request.session['cart'] = cart
+    return redirect('cart')
